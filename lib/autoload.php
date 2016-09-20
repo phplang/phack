9bfa4b9f33260e8c1036547b01250287b6fbd900
Sara Golemon <pollita@php.net>
2016-09-20 01:42:02 -0700

    Allow dumping raw AST from vendor/bin/phack
    vendor/bin/phack --ast filename.php
    
    Parse file and print_r() the AST.
    Without flag, still transpiles to PHP code.

--- lib/autoload.php
+++ lib/autoload.php
@@ -9,20 +9,27 @@ if (defined('PHACK_AUTOLOAD')) {
 }
 
 /**
+ * Parse and compile a HackLAng string (open tag required) into an AST tree
+ */
+function compileString($str) {
+    if (!strncmp($str, '<?php', 5)) {
+        return (new \PhpParser\ParserFactory)
+            ->create(\PhpParser\ParserFactory::PREFER_PHP7)
+            ->parse($str);
+    } else {
+        return (new PhpParser\ParserFactory)
+            ->create(PhpParser\ParserFactory::HACKLANG)
+            ->parse($str);
+    }
+}
+
+/**
  * Convert a HackLang script (open tag required) from HackLang to PHP
  * Allows PHP scripts to passthrough unchanged apart from below:
  * Removes open tag for eval() suitability
  */
 function transpileString($str) {
-    if (!strncmp($str, '<?php', 5)) {
-        return substr($str, 5);
-    }
-    $ret =
-      (new PhpParser\PrettyPrinter\HackLang)
-        ->prettyPrint((new PhpParser\ParserFactory)
-                         ->create(PhpParser\ParserFactory::HACKLANG)
-                         ->parse($str));
-    return $ret;
+    return (new PhpParser\PrettyPrinter\HackLang)->prettyPrint(compileString($str));
 }
 
 /**
